35f5c2616f4f85dd610513974a21403004c07857
Levindo Neto <levindogtn@gmail.com>
2018-08-07 11:05:14 -0300

    Add rest configurations

--- weenvoyerbot-service-app/src/main/java/com/weenvoyer/bot/Application.java
+++ weenvoyerbot-service-app/src/main/java/com/weenvoyer/bot/Application.java
@@ -1,8 +1,12 @@
 package com.weenvoyer.bot;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.weenvoyer.arch.WeenvoyerServicesRunner;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "com.weenvoyer")
--- weenvoyerbot-service-contract/src/main/java/com/weenvoyer/bot/contract/config/RestConfig.java
+++ weenvoyerbot-service-contract/src/main/java/com/weenvoyer/bot/contract/config/RestConfig.java
@@ -1,4 +1,22 @@
 package com.weenvoyer.bot.contract.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
 public class RestConfig {
+
+    @Bean
+    public RestTemplate restTemplate(ObjectMapper objectMapper) {
+        RestTemplate restTemplate = new RestTemplate();
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        messageConverter.setPrettyPrint(false);
+        messageConverter.setObjectMapper(objectMapper);
+        restTemplate.getMessageConverters().removeIf(m -> m.getClass().getName().equals(MappingJackson2HttpMessageConverter.class.getName()));
+        restTemplate.getMessageConverters().add(messageConverter);
+        return restTemplate;
+    }
 }
